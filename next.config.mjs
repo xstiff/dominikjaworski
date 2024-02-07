0b64f58d0e5cf0659181b41ae048051bf220c29e
xstiff <dominik7550@gmail.com>
2024-02-07 19:04:44 +0100

    next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    staticPageGenerationTimeout: 180, // wartość w sekundach
+  };
+  
+  export default nextConfig;
+  
